96dfc22817c86ef371f6c88b543943cae8f93fdc
BRAJESWAR GHOSH <brajeswar520@gmail.com>
2025-01-05 22:03:16 +0530

    partial fixed

--- Routes/articles.route.js
+++
@@ -1,9 +0,0 @@
-const express = require("express");
-
-const articlesRouter = express.Router();
-const articleControllers = require("../Controllers/articles.controllers");
-
-articlesRouter.route("/article/:cat/:Id")
-    .get(articleControllers.article)
-
-module.exports = articlesRouter;
--- Routes/index.route.js
+++ Routes/index.route.js
@@ -3,11 +3,9 @@ const express = require("express");
 const router = express.Router();
 const homeRouter = require("./home.route");
 const categoryRouter = require("./category.route");
-const articlesRouter = require("./articles.route");
 
 router.use("/MakeYourTrip", homeRouter);
 router.use("/MakeYourTrip", categoryRouter);
-router.use("/MakeYourTrip", articlesRouter);
 
 
 module.exports=router;
